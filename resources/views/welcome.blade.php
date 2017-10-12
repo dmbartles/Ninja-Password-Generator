19faa56337aa241ad624147340c46c0273a9863d
dmbartles <dmbartles@yahoo.com>
2017-10-11 21:06:07 -0400

    Update readme and welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Foobooks</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -79,15 +79,13 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Foobooks
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="http://p1.kodiakjetpack.club">About Me</a>
+                    <a href="http://p2.kodiakjetpack.club">Baby Names</a>
+                    <a href="http://p3.kodiakjetpack.club">Passwords</a>
                 </div>
             </div>
         </div>
